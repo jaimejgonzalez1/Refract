cf02a546edccb78a7a2c658626dbb5bd18c6c7ea
Rafael Alvarado <rafael.alvaradosanchez@loop.colum.edu>
2017-04-10 17:54:38 -0500

    add new materialize, fixed collapse issue

--- client/templates/student/studentregister.js
+++ client/templates/student/studentregister.js
@@ -25,6 +25,10 @@ Template.student_register.helpers({
     },
 });
 
+Template.student_register.rendered = function(){
+  $('.collapsible').collapsible();
+}
+
 Template.student_register.events({
     'submit .applyProjectSubmit'(event, wut) {
         event.preventDefault();
