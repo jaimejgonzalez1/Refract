a2fe3243824ff6c89a6fb913baa64acff61e0cd9
dylanglover <dylan@Dylans-MacBook-Pro.local>
2017-04-13 10:59:05 -0500

    Idk

--- client/templates/student/studentregister.js
+++ client/templates/student/studentregister.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import {
     Meteor
 } from 'meteor/meteor';
@@ -15,16 +14,20 @@ Skills = new Mongo.Collection(null);
 skillsCollection.forEach(function(skill) {
     Skills.insert(skill)
 });
-=======
-import { Meteor } from 'meteor/meteor';
-import { Template } from 'meteor/templating';
+
+import {
+    Meteor
+} from 'meteor/meteor';
+import {
+    Template
+} from 'meteor/templating';
 
 import './studentregister.html';
->>>>>>> master
+
 
 
 Template.student_register.helpers({
-<<<<<<< HEAD
+
     'parent_skills': function() {
         // console.log(Projects.find().count());
         return Skills.find({
@@ -439,44 +442,54 @@ function loadSkills() {
         }
     ];
 }
-=======
-    'student_name': function() {
-        return Meteor.user().username;
-    },
-    'blurb': function() {
-        return Meteor.user().profile.blurb;
-    },
-    'linkedInLink': function() {
-        return Meteor.user().profile.linkedInLink;
-    },
-    'profileLink': function() {
-        return Meteor.user().profile.profileLink;
-    },
+
+'student_name': function() {
+    return Meteor.user().username;
+},
+'blurb': function() {
+    return Meteor.user().profile.blurb;
+},
+'linkedInLink': function() {
+    return Meteor.user().profile.linkedInLink;
+},
+'profileLink': function() {
+    return Meteor.user().profile.profileLink;
+},
 });
 
-Template.student_register.rendered = function(){
-  $('.collapsible').collapsible();
+Template.student_register.rendered = function() {
+    $('.collapsible').collapsible();
 }
 
 Template.student_register.events({
-    'submit #saveBlurb'(evt, wut) {
+    'submit #saveBlurb' (evt, wut) {
         evt.preventDefault();
         var text = evt.target[0].value;
-        if (Meteor.users.update({_id: Meteor.userId()}, {$set:{"profile.blurb":text}})) {
-        };
+        if (Meteor.users.update({
+                _id: Meteor.userId()
+            }, {
+                $set: {
+                    "profile.blurb": text
+                }
+            })) {};
     },
-    'keyup #profileLink'(evt, wut) {
+    'keyup #profileLink' (evt, wut) {
         evt.preventDefault();
         var text = evt;
         console.log(text);
         // if (Meteor.users.update({_id: Meteor.userId()}, {$set:{"profile.blurb":text}})) {
         // };
     },
-    'keyup #linkedInLink'(evt, wut) {
+    'keyup #linkedInLink' (evt, wut) {
         evt.preventDefault();
         var text = evt.target.value;
         console.log(text);
-        Meteor.users.update({_id: Meteor.userId()}, {$set:{"profile.linkedInLink":text}});
+        Meteor.users.update({
+            _id: Meteor.userId()
+        }, {
+            $set: {
+                "profile.linkedInLink": text
+            }
+        });
     },
 });
->>>>>>> master
