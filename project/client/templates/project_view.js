4ca12cf66eabda30ca38b2b79ea3f7aca293b311
Alexander Shoup <alexshoup@gmail.com>
2017-04-27 09:54:13 -0500

    project view

--- project/client/templates/project_view.js
+++ project/client/templates/project_view.js
@@ -10,9 +10,25 @@ Template.project_view.helpers({
         return Projects.findOne(Session.get('open_project'));
     },
     'skills': function() {
-        console.log(this.skills);
-        console.log(Skills.findOne({}));
-        console.log(Skills.findOne({'id': {'$in': this.skills}}));
         return Skills.find({'id': {'$in': this.skills}});
+    },
+    'owner': function() {
+        Meteor.subscribe('Meteorusers', [this.owner_id]);
+        console.log(this.owner_id);
+        console.log(Meteor.users.findOne(this.owner_id));
+        return Meteor.users.findOne(this.owner_id);
+    }
+});
+
+Template.project_view.events({
+    "click [data-action='link']": function (evt) {
+    // set current project
+    // load template
+    console.log(this._id);
+    console.log(evt.currentTarget);
+    console.log(evt.currentTarget.dataset.template);
+    Session.set('selected_user', evt.currentTarget.dataset.profile);
+    Session.set('last_template', Session.get('template_loaded'));
+    Session.set('template_loaded', evt.currentTarget.dataset.template);
     }
 });
